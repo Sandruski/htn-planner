68113222281d0ed26b7771cb58392dc14f698bfb
Jose Antonio Escribano <joseantonioescribanoayllon@gmail.com>
2023-04-01 15:43:51 +0200

    - first attempt to run sdl2 + imgui in HTNDemo, it works but I had to copy the SDL2.dll manually, which sucks

--- premake5.lua
+++ premake5.lua
@@ -71,14 +71,33 @@ project "HTNDemo"
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("int/" .. outputdir .. "/%{prj.name}")
 
-    pchheader "pch.h"
-    pchsource "%{prj.name}/src/pch.cpp"
-    forceincludes "pch.h"
+    --pchheader "pch.h"
+    --pchsource "%{prj.name}/src/pch.cpp"
+    --forceincludes "pch.h"
 
-    files { "%{prj.name}/src/**.cpp", "%{prj.name}/src/**.h" }
+    files { "%{prj.name}/src/**.cpp", "%{prj.name}/src/**.h", 
+				-- imgui includes --
+				"ThirdParty/imgui-1.89.4/imconfig.h", 
+				"ThirdParty/imgui-1.89.4/imgui.cpp", 
+				"ThirdParty/imgui-1.89.4/imgui.h", 
+				"ThirdParty/imgui-1.89.4/imgui_demo.cpp", 
+				"ThirdParty/imgui-1.89.4/imgui_draw.cpp", 
+				"ThirdParty/imgui-1.89.4/imgui_internal.h", 
+				"ThirdParty/imgui-1.89.4/imgui_tables.cpp", 
+				"ThirdParty/imgui-1.89.4/imgui_widgets.cpp", 
+				-- imgui backend includes --
+				"ThirdParty/imgui-1.89.4/backends/imgui_impl_sdl2.h", 
+				"ThirdParty/imgui-1.89.4/backends/imgui_impl_sdl2.cpp", 
+				"ThirdParty/imgui-1.89.4/backends/imgui_impl_sdlrenderer.h", 
+				"ThirdParty/imgui-1.89.4/backends/imgui_impl_sdlrenderer.cpp" 
+		}
 
-    includedirs { "%{prj.name}/src", "HTN/src" }
+    includedirs { "%{prj.name}/src", "HTN/src", "ThirdParty/SDL2-2.26.4/include", "ThirdParty/imgui-1.89.4", "ThirdParty/imgui-1.89.4/backends" }
 
-    links { "HTN" }
-
-    nuget { "Microsoft.googletest.v140.windesktop.msvcstl.static.rt-dyn:1.8.1.7" }
+	libdirs { "ThirdParty/SDL2-2.26.4/lib/x64" }
+    links { "HTN", "SDL2", "SDL2main" }
+	
+	-- TODO JOSE: Make this work properly, we should copy the SDL2.dll in the right target dir, I am pretty sure there is a macro to do that.
+	-- filter "configurations:Debug"
+		-- postbuildcommands { "copy ThirdParty/SDL2-2.26.4/lib/x64/SDL2.dll bin\\Debug-windows-x86_64\\%{prj.name}\\" }
+	
